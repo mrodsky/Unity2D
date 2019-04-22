47b9e7d6d75ad2ed6afbba1c53e9eb4cf770e32b
michael <mrodsky@gmail.com>
2019-04-22 18:38:00 -0500

    fixed 1000 bug

--- Number_Wizard_Console/Number Wizard Console/Assets/NumberWizard.cs
+++ Number_Wizard_Console/Number Wizard Console/Assets/NumberWizard.cs
@@ -3,19 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class NumberWizard : MonoBehaviour {
-    private int max = 1000;
-    private int min = 1;
-    private int guess = 500;
+    private int max;
+    private int min;
+    private int guess;
     // Use this for initialization
     void Start ()
     {
+        greeting();
+   
+
+    }
+
+    public void greeting()
+    {
+        max = 1000;
+        min = 1;
+       guess = 500;
+
+
         Debug.Log("Welcome to Number Wizard");
         Debug.Log("Please Pick a Number: ");
         Debug.Log("The Highest Number you can Pick is " + max);
-        Debug.Log("The Lowest Number you can Pick is "+min);
+        Debug.Log("The Lowest Number you can Pick is " + min);
         Debug.Log("Tell me if you number is Higher or Lower then 500");
         Debug.Log("Push Up = higher, Push Down = lower , Push Enter = correct!");
-
+        max = max + 1;  
     }
 	
 	// Update is called once per frame
@@ -28,24 +40,30 @@ public class NumberWizard : MonoBehaviour {
         {
             Debug.Log("Up Arrow key was pressed.");
             min = guess;
-            guess = (max + min) / 2;
-            Debug.Log(guess);
+            NextGuess();
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             Debug.Log("Down Arrow key was pressed.");
             max = guess;
-            guess = (max + min) / 2;
-            Debug.Log(guess);
+            NextGuess();
         }
         else if (Input.GetKeyDown(KeyCode.Return))
         {
             Debug.Log("Enter key was pressed.");
             Debug.Log("Your Number is : " + guess);
+            greeting();
         }
         else if (Input.GetKeyDown(KeyCode.KeypadEnter))
         {
             Debug.Log("\"Other\" Enter key was pressed.");
+            greeting();
         }
+    }
+
+    void NextGuess()
+    {
+        guess = (max + min) / 2;
+        Debug.Log("Is it Higher or lower then.. " + guess);
     }
 }
